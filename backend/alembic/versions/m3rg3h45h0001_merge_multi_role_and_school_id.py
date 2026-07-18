1167c191b7202b5f5a7403d25d7f9fea00bdf486
John <john@zenova.et>
2026-07-18 14:27:41 +0300

    fix: add merge migration for multi_role and school_id heads

---
+++ backend/alembic/versions/m3rg3h45h0001_merge_multi_role_and_school_id.py
@@ -0,0 +1,23 @@
+"""merge_multi_role_and_school_id
+
+Revision ID: m3rg3h45h0001
+Revises: a0b1c2d3e4f5, d1e2f3a4b5c6d7e8
+Create Date: 2026-07-18 08:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = 'm3rg3h45h0001'
+down_revision = ('a0b1c2d3e4f5', 'd1e2f3a4b5c6d7e8')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
